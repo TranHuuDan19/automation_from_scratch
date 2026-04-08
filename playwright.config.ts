6f03012e5ca340968bebb0cb993e7b28be5758af
thdan19 <thdan19@thdan19s-mac.local>
2026-04-08 15:20:36 +0700

    chore: update ci file to cache deps

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,19 +3,19 @@ import env from './utils/config/env'
 
 export default defineConfig({
   outputDir: './test-results/artifacts/',
-  workers: process.env.CI ? 4 : 2,
-  retries: process.env.CI ? 3 : 0,
+  workers: process.env.CI ? '100%' : 2,
+  retries: process.env.CI ? 1 : 0,
   use: {
         headless: process.env.CI ? true : false,
         viewport: null,
         launchOptions: {
-          slowMo: process.env.CI ? 0 : 100,
+          slowMo: process.env.CI ? 0 : 10,
           args: ['--start-maximized']
         },
         baseURL: env.baseURL,
         screenshot: 'only-on-failure',
-        video: 'retain-on-failure',
-        trace: 'on-first-retry',  
+        video: process.env.CI ? 'off' : 'retain-on-failure',
+        trace: 'retain-on-failure',  
       },
   projects: [
     {
